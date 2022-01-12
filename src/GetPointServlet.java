355c8c30950ac43b2b207b185196c4cda465aa07
龍成 小林 <190736@jc-21.jp>
2022-01-12 09:54:12 +0900

    動作確認

--- src/GetPointServlet.java
+++ src/GetPointServlet.java
@@ -1,22 +1,13 @@
 
-import java.beans.Statement;
-import java.io.IOException;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.sql.PreparedStatement;
-import java.sql.Connection;
 
-import javax.naming.InitialContext;
+import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
-
 
 /**
  * Servlet implementation class GetPointServlet
@@ -24,40 +15,21 @@ import javax.sql.DataSource;
 @WebServlet("/getPoint")
 public class GetPointServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetPointServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
-	 * @see HttpServlet#HttpServlet()
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	public GetPointServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		try {
-			InitialContext ic = new InitialContext();
-			DataSource ds = (DataSource) ic.lookup("java:/");
-			Connection con = ds.getConnection();
-			con.setAutoCommit(false);
-			PreparedStatement st = con.prepareStatement("select * from aaa");
-			con.setAutoCommit(true);
-			
-			//処理
-
-			
-			st.close();
-			con.close();
-		} catch (Exception e) {
-			System.out.println("<pre>");
-			e.printStackTrace();
-		}
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
 		rd.forward(request, response);
 	}
+
 }
