1c037d9e68674a23008135925f5046d583ea4651
龍成 小林 <190736@jc-21.jp>
2022-02-18 11:25:50 +0900

    getticket完成

--- src/GetTicketListServlet.java
+++ src/GetTicketListServlet.java
@@ -30,10 +30,12 @@ public class GetTicketListServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		final String driverName = "com.mysql.jdbc.Driver";
 		final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai10";
 		final String id = "jsonkadai10";
@@ -43,50 +45,48 @@ public class GetTicketListServlet extends HttpServlet {
 			Class.forName(driverName);
 			Connection connection = DriverManager.getConnection(url, id, pass);
 
-			PreparedStatement st = connection.prepareStatement("select * from ticket where dept_id = ?");
-			PreparedStatement st1 = connection.prepareStatement("select * from point where user_id = ? and dept_id = ?");
+			PreparedStatement st = connection.prepareStatement("select * from ticket where dept_id = ? && point <= ?");
+			PreparedStatement st1 = connection
+					.prepareStatement("select * from point where user_id = ? and dept_id = ?");
 
 			String user = request.getParameter("USER_ID");
 			String tenpo = request.getParameter("TENPO_ID");
 
+
 			int point = 0;
-			
-			st.setString(1,tenpo);
-			
+
 			st1.setString(1, user);
 			st1.setString(2, tenpo);
-			
-			ResultSet rs = st.executeQuery();
+
 			ResultSet rs1 = st1.executeQuery();
-			
-			if(rs1.next() == true) {
+
+			if (rs1.next() == true) {
 				point = rs1.getInt("point");
-				
+
 			}
-			
+
+			st.setString(1, tenpo);
+			st.setInt(2, point);
+
+			ResultSet rs = st.executeQuery();
+
 			java.util.List<String[]> list = new ArrayList<>();
 			while (rs.next() == true) {
-				int pointo = rs.getInt("point");
 
 				String[] s = new String[3];
-				
-				if(pointo >= point) {
+
 				s[0] = rs.getString("ticket_id");
 				s[1] = rs.getString("ticket_name");
 				s[2] = rs.getString("point");
 				list.add(s);
-				}
 
 			}
 
-			
 			request.setAttribute("list", list);
-
-
 
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getTicketList.jsp");
 			rd.forward(request, response);
-				
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
